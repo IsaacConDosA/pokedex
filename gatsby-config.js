6b8ad8a4a13c8966253f77d44f8ec504261cfd61
Isaac Carrera <ifcarrera@uc.cl>
2022-03-01 01:31:39 -0300

    Updates

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('gatsby').GatsbyConfig} */
-module.exports = {
-  siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  pathPrefix: "/pokedex",
-  plugins: ["gatsby-plugin-styled-components"],
-};
