b3855b0e861b090ae8554e13cd98ba0d28a584d4
Isaac Carrera <ifcarrera@uc.cl>
2022-03-07 03:03:00 -0300

    chore: upload title icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,16 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   pathPrefix: "/pokedex",
-  plugins: ["gatsby-plugin-postcss"],
+  plugins: [
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Pokédex",
+        short_name: "pokedex",
+        start_url: "/",
+        icon: "src/images/favicon.png",
+      },
+    },
+  ],
 };
